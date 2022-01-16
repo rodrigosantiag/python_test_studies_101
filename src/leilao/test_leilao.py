b6b60f8cbae4489dfbcf62b52d5030c03cca5bec
Rodrigo Santiago <rodrigosantiag@users.noreply.github.com>
2022-01-16 12:24:53 -0300

    Implement new business logic which consists in not accept a lance lower than the last one

--- src/leilao/test_leilao.py
+++ src/leilao/test_leilao.py
@@ -7,15 +7,16 @@ class TestLeilao(TestCase):
 
     def setUp(self):
         self.gui = Usuario("Gui")
-        self.yuri = Usuario("Yuri")
 
-        self.lance_do_yuri = Lance(self.yuri, 100.0)
         self.lance_do_gui = Lance(self.gui, 150.0)
 
         self.leilao = Leilao("Celular")
 
     def test_deve_retorna_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
-        self.leilao.propoe(self.lance_do_yuri)
+        yuri = Usuario('Yuri')
+        lance_do_yuri = Lance(yuri, 100.0)
+
+        self.leilao.propoe(lance_do_yuri)
         self.leilao.propoe(self.lance_do_gui)
 
         menor_valor_esperado = 100.0
@@ -24,15 +25,13 @@ class TestLeilao(TestCase):
         self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
-    def test_deve_retorna_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_decrescente(self):
-        self.leilao.propoe(self.lance_do_gui)
-        self.leilao.propoe(self.lance_do_yuri)
+    def test_nao_deve_permitir_propor_um_lance_em_ordem_decrescente(self):
+        with self.assertRaises(ValueError):
+            yuri = Usuario('Yuri')
+            lance_do_yuri = Lance(yuri, 100.0)
 
-        menor_valor_esperado = 100.0
-        maior_valor_esperado = 150.0
-
-        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
-        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+            self.leilao.propoe(self.lance_do_gui)
+            self.leilao.propoe(lance_do_yuri)
 
     def test_deve_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_leilao_tiver_um_lance(self):
         self.leilao.propoe(self.lance_do_gui)
@@ -41,13 +40,14 @@ class TestLeilao(TestCase):
         self.assertEqual(150.0, self.leilao.maior_lance)
 
     def test_deve_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_leilao_tiver_tres_lances(self):
+        yuri = Usuario('Yuri')
         vini = Usuario("Vini")
-
+        lance_do_yuri = Lance(yuri, 100.0)
         lance_do_vini = Lance(vini, 200.0)
 
-        self.leilao.propoe(self.lance_do_yuri)
+        self.leilao.propoe(lance_do_yuri)
         self.leilao.propoe(self.lance_do_gui)
-        self.leilao.propoe(lance_do_vini)\
+        self.leilao.propoe(lance_do_vini)
 
         menor_valor_esperado = 100.0
         maior_valor_esperado = 200.0
@@ -63,8 +63,11 @@ class TestLeilao(TestCase):
         self.assertEqual(1, quantidade_de_lances_recebidos)
 
     def test_deve_permitir_propor_um_lance_caso_o_ultimo_usuario_seja_diferente(self):
+        yuri = Usuario('Yuri')
+        lance_do_yuri = Lance(yuri, 100.0)
+
+        self.leilao.propoe(lance_do_yuri)
         self.leilao.propoe(self.lance_do_gui)
-        self.leilao.propoe(self.lance_do_yuri)
 
         quantidade_de_lances_recebidos = len(self.leilao.lances)
 
