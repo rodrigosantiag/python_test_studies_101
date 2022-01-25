0bbc3baf53eefafe372387c4d6d78b8badf67ba1
Rodrigo Santiago <rodrigosantiag@users.noreply.github.com>
2022-01-25 15:13:33 -0300

    Refactor class without interfering in tests

--- src/leilao/dominio.py
+++ src/leilao/dominio.py
@@ -1,5 +1,3 @@
-import sys
-
 class Usuario:
 
     def __init__(self, nome, carteira):
@@ -15,7 +13,7 @@ class Usuario:
         return self.__carteira
 
     def propoe_lance(self, leilao, valor):
-        if valor > self.__carteira:
+        if self._valor_valido(valor):
             raise ValueError("Não pode propor um lance maior que o valor da carteira")
 
         lance = Lance(self, valor)
@@ -23,6 +21,9 @@ class Usuario:
         leilao.propoe(lance)
 
         self.__carteira -= valor
+
+    def _valor_valido(self, valor):
+        return valor > self.__carteira
 
 
 class Lance:
@@ -37,16 +38,15 @@ class Leilao:
     def __init__(self, descricao):
         self.descricao = descricao
         self.__lances = []
-        self.maior_lance = sys.float_info.min
-        self.menor_lance = sys.float_info.max
+        self.maior_lance = 0.0
+        self.menor_lance = 0.0
 
     def propoe(self, lance: Lance):
-        if not self.__lances or self.__lances[-1].usuario != lance.usuario and lance.valor > self.__lances[-1].valor:
-            if lance.valor > self.maior_lance:
-                self.maior_lance = lance.valor
-
-            if lance.valor < self.menor_lance:
+        if self._lance_valido(lance):
+            if not self._tem_lances():
                 self.menor_lance = lance.valor
+
+            self.maior_lance = lance.valor
 
             self.__lances.append(lance)
         else:
@@ -56,3 +56,15 @@ class Leilao:
     def lances(self):
         return self.__lances[:]
 
+    def _tem_lances(self):
+        return self.__lances
+
+    def _usuarios_diferentes(self, lance):
+        return self.__lances[-1].usuario != lance.usuario
+
+    def _valor_maior_que_lance_anterior(self, lance):
+        return lance.valor > self.__lances[-1].valor
+
+    def _lance_valido(self, lance):
+        return not self._tem_lances() or self._usuarios_diferentes(lance) and self._valor_maior_que_lance_anterior(
+            lance)
